2b4482150a9040dff24245da5d5ce609125de06d
Adyellen <adyelleenalves@gmail.com>
2020-07-02 19:01:02 -0300

    first commit

---
+++ os-algaworks-api/README.md
@@ -0,0 +1,8 @@
+# Curso de Spring Boot da Alga works 
+
+### OS (Ordem de Serviço)
+
+* API RestFull;
+* Postman;
+
+
---
+++ os-algaworks-api/src/test/java/com/algaworks/aayellen/OsAlgaworksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.aayellen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsAlgaworksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
